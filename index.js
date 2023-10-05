791dd94473b3a4442e2810653ea93e846f5f5162
Abdifatah <mohamedabdifatah93@gmail.com>
2023-10-05 14:51:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Abdifatah Mohamed';
+const welcomeMessage = 'Welcome to Flatbook, ${Abdifatah Mohamed}!'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting)
